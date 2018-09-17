63d181b5aaccc6b1636c8bc32c72569bff4405e6
Martin Hwang <dhhwang89@gmail.com>
2018-09-17 12:46:25 +0900

    1. train.py modified

--- pytorch_version/train.py
+++ pytorch_version/train.py
@@ -68,73 +68,81 @@ scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
 
 # Train the model
 total_step = len(train_loader)
-for epoch in range(num_epochs):
 
-    if (epoch == 200) or (epoch == 400) or (epoch == 600) or (epoch == 20000) or (epoch == 30000):
-        scheduler.step()
+with open("train_log.txt", "w") as f:
 
-    for i, (images, labels) in enumerate(train_loader):
+    for epoch in range(num_epochs):
 
-    
-        images = images.to(device)
-        labels = labels.to(device)
+        if (epoch == 200) or (epoch == 400) or (epoch == 600) or (epoch == 20000) or (epoch == 30000):
+            scheduler.step()
 
-        # Forward pass
-        outputs = model(images)
-
-        # Calc Loss
-        loss, \
-        obj_coord1_loss, \
-        obj_size1_loss, \
-        obj_coord2_loss, \
-        obj_size2_loss, \
-        obj_class_loss, \
-        noobj_class_loss, \
-        objness1_loss, \
-        objness2_loss = yolov1.detection_loss(outputs, labels)
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if (i + 1) % 10 == 0:
-
-            print('Epoch:[{}/{}], Step:[{}/{}], learning rate:{}\ttotal_loss\t{:.4f}\tcoord1\t{}\tsize1\t{}\tcoord2\t{}\tsize2\t{}\tclass\t{}\tnoobj_clss\t{}\tobjness1\t{}\tobjness2\t{}'
-                  .format(epoch + 1,
-                          num_epochs,
-                          i + 1,
-                          total_step,
-                          [param_group['lr'] for param_group in optimizer.param_groups],
-                          loss.item(),
-                          obj_coord1_loss,
-                          obj_size1_loss,
-                          obj_coord2_loss,
-                          obj_size2_loss,
-                          obj_class_loss,
-                          noobj_class_loss,
-                          objness1_loss,
-                          objness2_loss
-                          ))
-            
-
-            yolov1.update_vis_plot(viz, (epoch+1)*batch_size +(i + 1), loss.item(), iter_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_coord1_loss, coord1_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_size1_loss, size1_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_coord2_loss, coord2_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_size2_loss, size2_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_class_loss, obj_cls_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), noobj_class_loss, noobj_cls_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), objness1_loss, objectness1_plot, None, 'append')
-            yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), objness2_loss, objectness2_plot, None, 'append')
+        for i, (images, labels) in enumerate(train_loader):
 
 
-            
+            images = images.to(device)
+            labels = labels.to(device)
 
-    if (epoch % 300) == 0:
-        yolov1.save_checkpoint({
-            'epoch': epoch + 1,
-            'arch': "YOLOv1",
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-        }, False, filename='checkpoint_{}.pth.tar'.format(epoch))
+            # Forward pass
+            outputs = model(images)
+
+            # Calc Loss
+            loss, \
+            obj_coord1_loss, \
+            obj_size1_loss, \
+            obj_coord2_loss, \
+            obj_size2_loss, \
+            obj_class_loss, \
+            noobj_class_loss, \
+            objness1_loss, \
+            objness2_loss = yolov1.detection_loss(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i + 1) % 10 == 0:
+
+                log = ('Epoch:[{}/{}], Step:[{}/{}], learning rate:{}\ttotal_loss\t{:.4f}\tcoord1\t{}\tsize1\t{}\tcoord2\t{}\tsize2\t{}\tclass\t{}\tnoobj_clss\t{}\tobjness1\t{}\tobjness2\t{}\n'
+                      .format(epoch + 1,
+                              num_epochs,
+                              i + 1,
+                              total_step,
+                              [param_group['lr'] for param_group in optimizer.param_groups],
+                              loss.item(),
+                              obj_coord1_loss,
+                              obj_size1_loss,
+                              obj_coord2_loss,
+                              obj_size2_loss,
+                              obj_class_loss,
+                              noobj_class_loss,
+                              objness1_loss,
+                              objness2_loss
+                              ))
+
+                print(log)
+                f.write(log)
+
+
+                yolov1.update_vis_plot(viz, (epoch+1)*batch_size +(i + 1), loss.item(), iter_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_coord1_loss, coord1_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_size1_loss, size1_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_coord2_loss, coord2_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_size2_loss, size2_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), obj_class_loss, obj_cls_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), noobj_class_loss, noobj_cls_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), objness1_loss, objectness1_plot, None, 'append')
+                yolov1.update_vis_plot(viz, (epoch + 1) * batch_size + (i + 1), objness2_loss, objectness2_plot, None, 'append')
+
+
+
+
+        if (epoch % 300) == 0:
+            yolov1.save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': "YOLOv1",
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, False, filename='checkpoint_{}.pth.tar'.format(epoch))
+
+    f.close()
