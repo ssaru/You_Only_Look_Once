1. check origin yolo label  2. modified wrong grid cell index calculation fomula

pytorch_version/train.py
@@ -52,10 +52,10 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # VOC Pascal Dataset
 TT = "/home/keti-1080ti/Documents/dev/Yolov1/dataset/"
 DATASET_PATH_MARTIN_LAP = "/home/martin/Desktop/5class/_class_balance/"
-DATASET_PATH_MARTIN_COM = "/media/keti-ai/AI_HARD3/DataSets/VOC_Pascal/VOC/VOCdevkit/VOC2012"
+DATASET_PATH_MARTIN_COM = "/home/martin/Desktop/_class_balance/"
 DATASET_PATH_JAEWON = "D:\dataset\VOC2012"
 # transforms.ToTensor()
-train_dataset = VOC(root = DATASET_PATH_MARTIN_LAP,
+train_dataset = VOC(root = DATASET_PATH_MARTIN_COM,
                     transform=None, cls_option = False, selective_cls=None)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
@@ -86,9 +86,8 @@ with open("train_log.txt", "w") as f:
 
         for i, (images, labels) in enumerate(train_loader):
 
-            visualize_GT(images, labels, train_dataset.classes)
-
-            exit()
+            # GT Check
+            #visualize_GT(images, labels, train_dataset.classes)
 
             images = images.to(device)
             labels = labels.to(device)

pytorch_version/utilities/dataloader.py
@@ -137,30 +137,39 @@ class VOC(data.Dataset):
 
         target = self.data[index][key]
 
-        # for debug
-        print(img.shape)
-        #plt.imshow(img.astype(np.uint8))
-        #plt.show()
-        #exit()
+        # for debug & visualization
+        """
         _image = img.astype(dtype=np.uint8)
         _image = Image.fromarray(_image, "RGB")
         draw = ImageDraw.Draw(_image)
-        _dict = self.dict_data[key.split("/")[-1].split(".")[0]]
-        num_obj = int(_dict["objects"]["num_obj"])
-        print(_dict)
-        for i in range(num_obj):
-            xmin = int(_dict["objects"][str(i)]["bndbox"]["xmin"])
-            ymin = int(_dict["objects"][str(i)]["bndbox"]["ymin"])
-            xmax = int(_dict["objects"][str(i)]["bndbox"]["xmax"])
-            ymax = int(_dict["objects"][str(i)]["bndbox"]["ymax"])
-            cls = _dict["objects"][str(i)]["name"]
+
+
+        for obj in target:
+
+            cls = obj[0]
+            x_center = obj[1]
+            y_center = obj[2]
+            w_ratio = obj[3]
+            h_ratio = obj[4]
+
+            width = int(w_ratio * self.resize_factor)
+            height = int(h_ratio * self.resize_factor)
+
+            xmin = int(x_center * self.resize_factor - width/2)
+            ymin = int(y_center * self.resize_factor - height/2)
+            xmax = xmin + width
+            ymax = ymin + height
+
+            cls = self.classes[int(cls)]
+
             draw.rectangle(((xmin, ymin), (xmax, ymax)), outline="blue")
             draw.text((xmin, ymin), cls)
 
+
         plt.imshow(_image)
         plt.show()
+        """
 
-        #draw.rectangle(((xmin, ymin), (xmax, ymax)), outline="blue")
 
         if self.transform is not None:
             #img, target = self.transform([img, target])
@@ -182,13 +191,7 @@ class VOC(data.Dataset):
 
         else:
             img = torch.FloatTensor(img)
-            #print(img)
             img = torch.div(img, 255)
-            #print(img)
-            #print(img.shape)
-
-
-
 
         if self.target_transform is not None:
             # Future works

pytorch_version/utilities/dataloader_test.py
@@ -28,15 +28,22 @@ def detection_collate(batch):
             h_ratio = object[4]
 
             # can be acuqire grid (x,y) index when divide (1/S) of x_ratio
-            grid_x_index = int(x_ratio // (1/7))
-            grid_y_index = int(y_ratio // (1/7))
-            x_offset = x_ratio - ((grid_x_index) * (1/7))
-            y_offset = y_ratio - ((grid_y_index) * (1/7))
+            scale_factor = (1/7)
+            grid_x_index = int(x_ratio // scale_factor)
+            grid_y_index = int(y_ratio // scale_factor)
+            x_offset = (x_ratio / scale_factor) - grid_x_index
+            y_offset = (y_ratio / scale_factor) - grid_y_index
+
+            #print("x ratio : {}, y ratio : {}".format(x_ratio, y_ratio))
+            #print("x index : {}, y index : {}".format(grid_x_index, grid_y_index))
+            #print("x shift : {}, y shift : {}".format(x_offset, y_offset))
+            #print("x raw : {}, y raw : {}".format(x_ratio/scale_factor, y_ratio/scale_factor))
+
 
             # insert object row in specific label tensor index as (x,y)
             # object row follow as
             # [objectness, class, x offset, y offset, width ratio, height ratio]
-            np_label[grid_x_index-1][grid_y_index-1] = np.array([objectness, cls, x_offset, y_offset, w_ratio, h_ratio])
+            np_label[grid_x_index][grid_y_index] = np.array([objectness, cls, x_offset, y_offset, w_ratio, h_ratio])
 
         label = torch.from_numpy(np_label)
         targets.append(label)

pytorch_version/utilities/utils.py
@@ -61,7 +61,7 @@ def detection_collate_with_size(batch):
             # object row follow as
             # [objectness, class, x offset, y offset, width ratio, height ratio]
             np_label[grid_x_index-1][grid_y_index-1] = np.array([objectness, cls, x_offset, y_offset, w_ratio, h_ratio])
-            
+
         label = torch.from_numpy(np_label)
         targets.append(label)
 
@@ -166,40 +166,13 @@ def visualize_GT(images, labels, cls_list):
 
     assert (Ibatch == Lbatch)
 
-    print(labels)
-
-    exit()
-    # For Debug
-    """
-    print(cls_list)
-    print("=======================================================")
-    print(images)
-
-    print("=======================================================")
-
-    print(images)
-    print("=======================================================")
-    """
     images = torch.mul(images, 255)
 
     for i in range(Ibatch):
         image = images.type(torch.ByteTensor)
 
-
-        # for Debug
-        """
-        print("=======================================================")
-        print(type(image))
-        print(image.shape)
-        """
         img = image[i, :, :, :].cpu().data.numpy()
         label = labels[i, :, :].cpu().data.numpy()
-
-        # for Debug
-        """
-        print(img)
-        print("=======================================================")
-        """
 
         # Draw 7x7 Grid in Image
         dx = Iw // 7
@@ -210,8 +183,8 @@ def visualize_GT(images, labels, cls_list):
         img[:,:: dy] = color
         img[::dx,:] = color
 
-        print(label.shape)
-        print(label[:,:,0])
+        #print(label.shape)
+        #print(label[:,:,0])
 
         obj_coord = label[:,:,0]
         cls = label[:,:,1]
@@ -222,8 +195,6 @@ def visualize_GT(images, labels, cls_list):
 
         _img= Image.fromarray(img, 'RGB')
         draw = ImageDraw.Draw(_img)
-
-
 
         for i in range(7):
             for j in range(7):
@@ -251,6 +222,3 @@ def visualize_GT(images, labels, cls_list):
         plt.imshow(_img)
         plt.show()
         plt.close()
-
-
-    pass